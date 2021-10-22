901aa23e3661584c50841aa01952334820ce06e8
Neetu-24 <87630879+Neetu-24@users.noreply.github.com>
2021-10-22 08:50:44 +0530

    Add files via upload
    new files added

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import os
+from PIL import Image, ImageOps
+import utils
+
+st.title("Image Super Resolution")
+st.text("Insert the image below")
+
+
+#creating file uploader
+image_file = st.file_uploader(label = '', type = ['png'])
+
+INPUT_PATH = "./input.png"
+OUT_PATH = "./output.png"
+
+if image_file is None:
+    try:
+        os.remove(INPUT_PATH)
+        os.remove(OUT_PATH)
+
+    except FileNotFoundError:
+        st.success("No previous images.")
+
+else:
+    in_img = Image.open(image_file)
+    
+    #saving the input image in my local disk
+    with open(INPUT_PATH, "wb") as file:
+        file.write(image_file.getbuffer())
+
+
+
+if st.button("Process"):
+    st.header("Low resolution image is:")
+    st.image(in_img, width = 300)
+
+    out_img = utils.get_prediction(INPUT_PATH)
+    out_img.save(OUT_PATH)
+
+    st.header("High resolution image is:")
+    st.image(out_img, width = 300)
+
+
+    
+
